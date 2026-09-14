fc9a6c9a04e701940ccbab85874ce2774e77ceef
jammingend <jaimeyregeling@gmail.com>
2026-09-14 22:49:51 +0200

    rework Pocket Pocket Dimension: heal 6 on last-enemy kill with consumption card

--- InterloperCode/Relics/PocketPocketDimensionRelic.cs
+++ InterloperCode/Relics/PocketPocketDimensionRelic.cs
@@ -1,11 +1,40 @@
+using Interloper.InterloperCode.Cards;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.Entities.Creatures;
 using MegaCrit.Sts2.Core.Entities.Relics;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
 
 namespace Interloper.InterloperCode.Relics;
 
-// cards with ethereal wont be brought back from your exhaust pile
+// when the player kills the last enemy with a card that has a consumption mechanic, heal 6 hp
 public class PocketPocketDimensionRelic : InterloperRelic
 {
     public override RelicRarity Rarity =>
         RelicRarity.Uncommon;
-    // edit the function that selects the cards form the exhaust pile
+
+    private bool _isConsumptionCardPlay;
+
+    public override async Task BeforeCardPlayed(CardPlay cardPlay)
+    {
+        _isConsumptionCardPlay = cardPlay.Card is CorruptionHandlerCard && cardPlay.Card.Owner == Owner;
+    }
+
+    public override async Task AfterCardPlayed(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+    {
+        _isConsumptionCardPlay = false;
+    }
+
+    public override async Task AfterDeath(PlayerChoiceContext choiceContext, Creature creature, bool wasRemovalPrevented, float deathAnimLength)
+    {
+        if (!_isConsumptionCardPlay)
+            return;
+
+        bool enemiesLeft = creature.CombatState?.HittableEnemies.Any(e => e != creature) ?? false;
+        if (enemiesLeft)
+            return;
+
+        _isConsumptionCardPlay = false;
+        await CreatureCmd.Heal(Owner.Creature, 6, true);
+    }
 }

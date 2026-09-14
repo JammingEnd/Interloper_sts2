ea67037a0919fb20cbe4c0f19078ce3f7ae2228f
jammingend <jaimeyregeling@gmail.com>
2026-09-14 16:47:41 +0200

    feat(potential): darken clear button on hover

--- InterloperCode/Nodes/NDarkPotentialClearButton.cs
+++ InterloperCode/Nodes/NDarkPotentialClearButton.cs
@@ -5,14 +5,27 @@ namespace Interloper.InterloperCode.Nodes;
 public partial class NDarkPotentialClearButton : TextureButton
 {
     private const float RotationSpeed = 0.4f;
+    private static readonly Color HoverColor = new(0.7f, 0.7f, 0.7f);
 
     public override void _Ready()
     {
         PivotOffset = Size * 0.5f;
+        MouseEntered += OnHovered;
+        MouseExited += OnUnhovered;
     }
 
     public override void _Process(double delta)
     {
         Rotation -= RotationSpeed * (float)delta;
+    }
+
+    private void OnHovered()
+    {
+        SelfModulate = HoverColor;
+    }
+
+    private void OnUnhovered()
+    {
+        SelfModulate = Colors.White;
     }
 }

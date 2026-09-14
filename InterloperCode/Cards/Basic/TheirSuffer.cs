f1d87dacf7547d2451229f08cbfd4c7f73291ab4
jammingend <jaimeyregeling@gmail.com>
2026-09-14 22:06:09 +0200

    loc: add Their Suffer card entries

---
+++ InterloperCode/Cards/Basic/TheirSuffer.cs
@@ -0,0 +1,25 @@
+using Interloper.InterloperCode.Cards;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+
+namespace Interloper.InterloperCode.Cards.Basic;
+
+public class TheirSuffer() : InterloperCard(int,
+    CardType.Attack, CardRarity.Basic,
+    TargetType.Self)
+{
+    protected override IEnumerable<DynamicVar> CanonicalVars => [];
+
+    protected override async Task OnPlay(
+        PlayerChoiceContext choiceContext,
+        CardPlay play)
+    {
+        
+    }
+
+    protected override void OnUpgrade()
+    {
+
+    }
+}

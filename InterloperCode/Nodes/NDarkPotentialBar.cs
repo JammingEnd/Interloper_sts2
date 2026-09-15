1b04a264fb93684d2818599468be1abbeec0b889
jammingend <jaimeyregeling@gmail.com>
2026-09-15 20:48:39 +0200

    feat(potential): lower potential counter by 7px

--- InterloperCode/Nodes/NDarkPotentialBar.cs
+++ InterloperCode/Nodes/NDarkPotentialBar.cs
@@ -106,7 +106,7 @@ public partial class NDarkPotentialBar : Control
             HorizontalAlignment = HorizontalAlignment.Center,
             VerticalAlignment = VerticalAlignment.Center,
             Size = new Vector2(BarSize.X, 22f),
-            Position = new Vector2(0f, Size.Y * 0.5f + 8f),
+            Position = new Vector2(0f, Size.Y * 0.5f + 1f),
             LabelSettings = new LabelSettings
             {
                 FontSize = 16,

989821c473b0c54c9e47f550abb77abf87acbabe
jammingend <jaimeyregeling@gmail.com>
2026-09-15 21:02:27 +0200

    rework Empower Curses: Weak a random enemy on block break, else Temporary Strength

--- InterloperCode/Powers/EmpowerCursePower.cs
+++ InterloperCode/Powers/EmpowerCursePower.cs
@@ -4,28 +4,41 @@ using MegaCrit.Sts2.Core.Commands;
 using MegaCrit.Sts2.Core.Entities.Creatures;
 using MegaCrit.Sts2.Core.Entities.Powers;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Models.Powers;
 
 namespace Interloper.InterloperCode.Cards.Uncommon;
 
 public class EmpowerCursePower : InterloperPower
 {
+    private bool _blockBroke;
+
     public override PowerType Type => PowerType.Buff;
     public override PowerStackType StackType => PowerStackType.Counter;
     public override PowerInstanceType InstanceType => PowerInstanceType.Instanced;
 
     public override async Task AfterBlockBroken(PlayerChoiceContext choiceContext, Creature target, Creature? breaker)
     {
-       
-        if (target != Owner && target.Player == null)
+        if (target != Owner || _blockBroke)
             return;
-        await PlayerCmd.GainEnergy(this.Amount, target.Player);
+
+        _blockBroke = true;
+
+        var enemies = target.CombatState?.HittableEnemies.ToArray() ?? [];
+        if (enemies.Length == 0)
+            return;
+
+        var weakTarget = target.Player.RunState.Rng.CombatTargets.NextItem(enemies);
+        await PowerCmd.Apply<WeakPower>(choiceContext, weakTarget, 1, target, null);
     }
 
     public override async Task BeforeSideTurnEndEarly(PlayerChoiceContext choiceContext, CombatSide side, IEnumerable<Creature> participants)
     {
-        if (side == CombatSide.Player)
-        {
-            await PowerCmd.Remove(this);
-        }
+        if (side != CombatSide.Player)
+            return;
+
+        if (!_blockBroke)
+            await PowerCmd.Apply<TemporaryStrengthPower>(choiceContext, Owner, 1, Owner, null);
+
+        await PowerCmd.Remove(this);
     }
 }
